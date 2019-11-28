5a70c5ee2c70a9a518f4089505bd4cba84111ab0
Dani <windowsdani@hotmail.com>
2019-11-28 14:55:19 +0100

    ñ

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const app = require('./app');
 mongoose.set('useCreateIndex', true);
 
-const mongodb = 'mongodb://localhost:27017/api_hackatron';
+const mongodb = 'mongodb://localhost:27017/api_hackaton';
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/api_hackatron', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect('mongodb://localhost:27017/api_hackaton', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) console.log(`ERROR: connecting to Database. ${err}`);
   else app.listen(port, console.log(`API started on: http://localhost:${port}`));
 });
